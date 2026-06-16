4f3db3f34054eece187c7928a8acf9a3471b0f69
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-15 19:06:24 -0600

    Add basic machine button bindings

--- scripts/ui/main_controller.gd
+++ scripts/ui/main_controller.gd
@@ -1,12 +1,19 @@
 extends Control
 
-@onready var add_node_button := get_node("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddNodeButton") as Button
 @onready var graph_view := get_node("Root/Body/ProductionGraphView") as GraphEdit
 
 func _ready() -> void:
-	add_node_button.pressed.connect(_on_add_node_button_pressed)
+	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddResourceSourceButton", "Resource Source", "In: none", "Out: Iron Ore")
+	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddCrusherButton", "Crusher", "In: Iron Ore", "Out: Crushed Iron Ore")
+	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddWasherButton", "Washer", "In: Crushed Iron Ore", "Out: Washed Iron Ore")
+	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddSmelterButton", "Smelter", "In: Washed Iron Ore", "Out: Iron Ingot")
 
 
-func _on_add_node_button_pressed() -> void:
+func _bind_button(button_path: String, machine_display_name: String, input_port_label: String, output_port_label: String) -> void:
+	var button := get_node(button_path) as Button
+	button.pressed.connect(_on_add_machine_pressed.bind(machine_display_name, input_port_label, output_port_label))
+
+
+func _on_add_machine_pressed(machine_display_name: String, input_port_label: String, output_port_label: String) -> void:
 	if graph_view.has_method("add_placeholder_machine_node"):
-		graph_view.call("add_placeholder_machine_node", "Processor", "In: input", "Out: output")
+		graph_view.call("add_placeholder_machine_node", machine_display_name, input_port_label, output_port_label)
