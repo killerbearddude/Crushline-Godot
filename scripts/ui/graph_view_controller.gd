900bff96cb919ba38e7e5aea8c2bfb01ddbb4499
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-15 19:35:26 -0600

    Show graph status in diagnostics (#10)
    * Emit graph status updates
    
    * Show graph status in diagnostics panel

--- scripts/ui/graph_view_controller.gd
+++ scripts/ui/graph_view_controller.gd
@@ -1,5 +1,7 @@
 extends GraphEdit
 
+signal status_text_changed(text: String)
+
 const TEST_NODE_NAME := "TestResourceSourceNode"
 const MACHINE_GRAPH_NODE_SCENE_PATH := "res://scenes/graph/MachineGraphNode.tscn"
 const PORT_TYPE_RESOURCE := 0
@@ -12,6 +14,8 @@ func _ready() -> void:
 	add_valid_right_disconnect_type(PORT_TYPE_RESOURCE)
 	connection_request.connect(_on_connection_request)
 	disconnection_request.connect(_on_disconnection_request)
+	node_selected.connect(_on_node_selected)
+	node_deselected.connect(_on_node_deselected)
 	_create_test_resource_source_node()
 
 
@@ -20,23 +24,55 @@ func add_placeholder_machine_node(machine_display_name: String, input_port_label
 	var node_name := "%sNode%d" % [machine_display_name.replace(" ", ""), next_node_index]
 	var position := Vector2(360.0 + (next_node_index * 40.0), 120.0 + (next_node_index * 30.0))
 	_create_machine_node(node_name, machine_display_name, input_port_label, output_port_label, position)
+	status_text_changed.emit(_describe_graph_status())
+
+
+func describe_graph_status() -> String:
+	return _describe_graph_status()
 
 
 func _on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	if from_node == to_node:
-		print("Connection rejected: a node cannot connect to itself.")
+		var rejected_text := "Connection rejected: a node cannot connect to itself.\n%s[%d] -> %s[%d]" % [from_node, from_port, to_node, to_port]
+		print(rejected_text)
+		status_text_changed.emit(rejected_text)
 		return
 
 	if is_node_connected(from_node, from_port, to_node, to_port):
+		status_text_changed.emit("Connection already exists:\n%s[%d] -> %s[%d]" % [from_node, from_port, to_node, to_port])
 		return
 
 	connect_node(from_node, from_port, to_node, to_port)
-	print("Connection added: %s[%d] -> %s[%d]" % [from_node, from_port, to_node, to_port])
+	var added_text := _describe_connection("Connection added", from_node, from_port, to_node, to_port)
+	print(added_text)
+	status_text_changed.emit(added_text)
 
 
 func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	disconnect_node(from_node, from_port, to_node, to_port)
-	print("Connection removed: %s[%d] -> %s[%d]" % [from_node, from_port, to_node, to_port])
+	var removed_text := _describe_connection("Connection removed", from_node, from_port, to_node, to_port)
+	print(removed_text)
+	status_text_changed.emit(removed_text)
+
+
+func _on_node_selected(node: Node) -> void:
+	var graph_node := node as GraphNode
+	if graph_node == null:
+		return
+
+	status_text_changed.emit(
+		"Selected node: %s\nScene name: %s\nPosition: %s\nInput: %s\nOutput: %s" % [
+			graph_node.title,
+			graph_node.name,
+			graph_node.position_offset,
+			graph_node.get("input_port_label"),
+			graph_node.get("output_port_label"),
+		]
+	)
+
+
+func _on_node_deselected(_node: Node) -> void:
+	status_text_changed.emit(_describe_graph_status())
 
 
 func _create_test_resource_source_node() -> void:
@@ -56,3 +92,19 @@ func _create_machine_node(node_name: String, machine_display_name: String, input
 	graph_node.set("output_port_label", output_port_label)
 
 	add_child(graph_node)
+
+
+func _describe_connection(action: String, from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> String:
+	return "%s:\n%s[%d] -> %s[%d]\nConnections: %d" % [action, from_node, from_port, to_node, to_port, get_connection_list().size()]
+
+
+func _describe_graph_status() -> String:
+	return "Graph status:\nNodes: %d\nConnections: %d\nSelect a node or create/remove a connection to inspect it." % [_count_graph_nodes(), get_connection_list().size()]
+
+
+func _count_graph_nodes() -> int:
+	var count := 0
+	for child in get_children():
+		if child is GraphNode:
+			count += 1
+	return count
--- scripts/ui/main_controller.gd
+++ scripts/ui/main_controller.gd
@@ -1,12 +1,19 @@
 extends Control
 
 @onready var graph_view := get_node("Root/Body/ProductionGraphView") as GraphEdit
+@onready var diagnostics_label := get_node("Root/DiagnosticsPanel/DiagnosticsLabel") as Label
 
 func _ready() -> void:
 	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddResourceSourceButton", "Resource Source", "In: none", "Out: Iron Ore")
 	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddCrusherButton", "Crusher", "In: Iron Ore", "Out: Crushed Iron Ore")
 	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddWasherButton", "Washer", "In: Crushed Iron Ore", "Out: Washed Iron Ore")
 	_bind_button("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddSmelterButton", "Smelter", "In: Washed Iron Ore", "Out: Iron Ingot")
+
+	if graph_view.has_signal("status_text_changed"):
+		graph_view.connect("status_text_changed", _on_graph_status_text_changed)
+
+	if graph_view.has_method("describe_graph_status"):
+		diagnostics_label.text = graph_view.call("describe_graph_status")
 
 
 func _bind_button(button_path: String, machine_display_name: String, input_port_label: String, output_port_label: String) -> void:
@@ -17,3 +24,7 @@ func _bind_button(button_path: String, machine_display_name: String, input_port_
 func _on_add_machine_pressed(machine_display_name: String, input_port_label: String, output_port_label: String) -> void:
 	if graph_view.has_method("add_placeholder_machine_node"):
 		graph_view.call("add_placeholder_machine_node", machine_display_name, input_port_label, output_port_label)
+
+
+func _on_graph_status_text_changed(text: String) -> void:
+	diagnostics_label.text = text
