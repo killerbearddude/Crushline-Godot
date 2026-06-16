43e125d053dea779e0e06ce9bb4502883465bfa6
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-15 18:45:14 -0600

    Use generic add node button

--- scripts/ui/main_controller.gd
+++ scripts/ui/main_controller.gd
@@ -1,12 +1,12 @@
 extends Control
 
-@onready var add_crusher_button := get_node("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddCrusherButton") as Button
+@onready var add_node_button := get_node("Root/Body/MachineLibraryPanel/MachineLibraryContents/AddNodeButton") as Button
 @onready var graph_view := get_node("Root/Body/ProductionGraphView") as GraphEdit
 
 func _ready() -> void:
-	add_crusher_button.pressed.connect(_on_add_crusher_button_pressed)
+	add_node_button.pressed.connect(_on_add_node_button_pressed)
 
 
-func _on_add_crusher_button_pressed() -> void:
+func _on_add_node_button_pressed() -> void:
 	if graph_view.has_method("add_placeholder_machine_node"):
-		graph_view.call("add_placeholder_machine_node", "Crusher", "In: Iron Ore", "Out: Crushed Iron Ore")
+		graph_view.call("add_placeholder_machine_node", "Processor", "In: input", "Out: output")
