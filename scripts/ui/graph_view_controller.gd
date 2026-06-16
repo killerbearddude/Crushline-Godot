625713fabeb0802c2e92759f981c37bce8b39f13
killerbearddude <58448059+killerbearddude@users.noreply.github.com>
2026-06-15 19:20:09 -0600

    Handle graph connection requests

--- scripts/ui/graph_view_controller.gd
+++ scripts/ui/graph_view_controller.gd
@@ -2,10 +2,16 @@ extends GraphEdit
 
 const TEST_NODE_NAME := "TestResourceSourceNode"
 const MACHINE_GRAPH_NODE_SCENE_PATH := "res://scenes/graph/MachineGraphNode.tscn"
+const PORT_TYPE_RESOURCE := 0
 
 var next_node_index := 0
 
 func _ready() -> void:
+	add_valid_connection_type(PORT_TYPE_RESOURCE, PORT_TYPE_RESOURCE)
+	add_valid_left_disconnect_type(PORT_TYPE_RESOURCE)
+	add_valid_right_disconnect_type(PORT_TYPE_RESOURCE)
+	connection_request.connect(_on_connection_request)
+	disconnection_request.connect(_on_disconnection_request)
 	_create_test_resource_source_node()
 
 
@@ -14,6 +20,23 @@ func add_placeholder_machine_node(machine_display_name: String, input_port_label
 	var node_name := "%sNode%d" % [machine_display_name.replace(" ", ""), next_node_index]
 	var position := Vector2(360.0 + (next_node_index * 40.0), 120.0 + (next_node_index * 30.0))
 	_create_machine_node(node_name, machine_display_name, input_port_label, output_port_label, position)
+
+
+func _on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
+	if from_node == to_node:
+		print("Connection rejected: a node cannot connect to itself.")
+		return
+
+	if is_node_connected(from_node, from_port, to_node, to_port):
+		return
+
+	connect_node(from_node, from_port, to_node, to_port)
+	print("Connection added: %s[%d] -> %s[%d]" % [from_node, from_port, to_node, to_port])
+
+
+func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
+	disconnect_node(from_node, from_port, to_node, to_port)
+	print("Connection removed: %s[%d] -> %s[%d]" % [from_node, from_port, to_node, to_port])
 
 
 func _create_test_resource_source_node() -> void:
